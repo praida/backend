fcca83683fa6f16a9f12f743e072a37a570ac634
Shawn Inder <shawn@vigour.io>
2017-08-21 20:03:25 -0400

    Log hash of basic auth `users` option to make sure we have the correct values

--- server/getUsers.js
+++ server/getUsers.js
@@ -1,5 +1,8 @@
+const hash = require('quick-hash')
+
 module.exports = exports = () => {
   const users = {}
   users[process.env.PRAIDA_PROD_USER] = process.env.PRAIDA_PROD_PASS
+  console.log('users', hash(JSON.stringify(users), 1))
   return { users }
 }
